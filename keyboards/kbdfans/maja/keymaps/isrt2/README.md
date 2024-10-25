fef3261968fc863bcafdde9bd555b26dceb1b512
Ark'Anoryn <arkanoryn@users.noreply.github.com>
2024-10-25 12:25:30 +0200

    Clean up keymap and update todo list
    Clean up the keymaps by adding new defines to reduce the size of the layout.
    
    Add and check the todos in the Readme.

--- keyboards/kbdfans/maja/keymaps/isrt2/README.md
+++ keyboards/kbdfans/maja/keymaps/isrt2/README.md
@@ -2,13 +2,18 @@
 
 - [X] Accents for MacOS.
 - [X] Move TAB to DF instead of current spot
-- [ ] Change the fancy LT etc keycode to define for cleaner keymap
+- [X] Change the fancy LT etc keycode to define for cleaner keymap
+- [X] Add the circonflexe on the symbol layer
+- [ ] Fix the EURO Keycaps
+- [ ] CTRL+ALT+SUPP shortcut for winOS
+- [ ] Lock / Unlock current layer
 - [ ] Look after the Comb, chords etc
+- [ ] Reduce the times between input and actions from leader
+- [ ] Add new lead functions (close tab, new private win, CTRL+1-5, smileys)
 - [ ] add the keydrawer PNG to the README
-- [ ] Rwork the home README
+- [ ] Rework the home README
 - [ ] Add licenses etc
 - [ ] Redo the combos for Graphite
 - [ ] Redo the leaders for Graphite
 - [ ] Manage RGB
 - [ ] Manage OLED screen
-- [ ] CTRL+ALT+SUPP shortcut for winOS
--- keyboards/kbdfans/maja/keymaps/isrt2/keymap.c
+++ keyboards/kbdfans/maja/keymaps/isrt2/keymap.c
@@ -4,6 +4,7 @@
 #define _____	KC_TRNS
 
 #include "./user/enums.h"
+#include "./keymap.h"
 #include "./user/commands/shortcuts.c"
 #include "./user/commands/accents.c"
 #include "./user/combos.c"
@@ -13,32 +14,32 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[ISRT] = LAYOUT(
-		__X__,		__X__,			__X__,			__X__,			__X__,			__X__,	__X__,	/* | */		__X__,			__X__,			__X__,			__X__,				__X__,			__X__,			    			__X__,		/* | */			TO(QWER),
-		__X__,		KC_Y,			LCTL_T(KC_C),	LALT_T(KC_L),	LGUI_T(KC_M),	KC_K,			/* | */		KC_Z,			RGUI_T(KC_F),	RALT_T(KC_U),	RCTL_T(KC_COMM), 	KC_QUOT, 		__X__,	 		    __X__, 		__X__,		/* | */			TO(NPD),
-		__X__,		LT(NPD, KC_I),	LT(ACCT, KC_S),	LT(SMBL, KC_R),	LT(ACT, KC_T),	KC_G,			/* | */		KC_P,			LT(ACT, KC_N),	LT(SMBL, KC_E),	LT(ACCT, KC_A),		LT(NPD, KC_O),	__X__,			    __X__,					/* | */			DF(GAME),
-		__X__,		LSFT_T(KC_Q),	KC_V,			KC_W,			KC_D,			KC_J,			/* | */		KC_B,			KC_H,			KC_SLSH,		KC_DOT,				RSFT_T(KC_X),	__X__,				__X__,					/* | */			__X__,
-		__X__,										__X__,			MEH_T(KC_SPC),	QK_LEAD,		/* | */		KC_BSPC,		KC_ENT,																				    				/* | */		__X__,		__X__,		__X__
+		__X__,  __X__,	__X__,	__X__,	__X__,	__X__,  __X__,	/* | */		__X__,		__X__,		__X__,		__X__,		__X__,		__X__,			__X__,	/* | */		TO(QWER),
+		__X__,  KC_Y,	CTL_C,	ALT_L,	GUI_M,	KC_K,			/* | */		KC_Z,		GUI_F,		ALT_U,		CTL_COMM,	KC_QUOT,	__X__,	__X__,	__X__,	/* | */		TO(NPD),
+		__X__,  L4_I,	L3_S,	L2_R,	L1_T,	KC_G,			/* | */		KC_P,		L1_N,		L2_E,		L3_A,		L4_O,		__X__,	__X__,			/* | */		TO(GAME),
+		__X__,  SFT_Q,	KC_V,	KC_W,	KC_D,	KC_J,			/* | */		KC_B,		KC_H,		KC_SLSH,	KC_DOT,		SFT_X,		__X__,	__X__,			/* | */		__X__,
+		__X__,  __X__,				   MEH_SPC,	QK_LEAD,		/* | */		KC_BSPC,	KC_ENT,														/* | */		__X__,		__X__,		__X__
 	),
 	[QWER] = LAYOUT(
-		QK_GESC,	KC_1,			KC_2,			KC_3,			KC_4,			KC_5,	KC_6,	/* | */		KC_7,			KC_8,			KC_9,			KC_0,				KC_MINS,		    KC_EQL,						KC_BSPC,	/* | */			KC_DEL,
-		KC_TAB,		KC_Q,			LCTL_T(KC_W),	LALT_T(KC_E),	LGUI_T(KC_R),	KC_T,			/* | */		KC_Y,			RGUI_T(KC_U),	RALT_T(KC_I),	RCTL_T(KC_O), 		KC_P, 			    KC_LBRC, 		KC_RBRC,	KC_BSLS,	/* | */			KC_HOME,
-		KC_CAPS,	LT(NPD, KC_A),  LT(ACCT, KC_S),	LT(SMBL, KC_D),	LT(ACT, KC_F),	KC_G,			/* | */		KC_H,			LT(ACT, KC_J),	LT(SMBL, KC_K),	LT(ACCT, KC_L),		LT(NPD, KC_SCLN),   KC_QUOT,		KC_ENT,					/* | */			KC_END,
-		KC_LSFT,	KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,			/* | */		TO(ISRT),		KC_N,			KC_M,			KC_COMM,			KC_DOT,			    KC_SLSH,		KC_RSFT,				/* | */			KC_UP,
-		KC_LCTL,									KC_LGUI,		MEH_T(KC_SPC),	KC_LALT,		/* | */		KC_SPC,			KC_RCTL,																    							/* | */		KC_LEFT,	KC_DOWN,	KC_RGHT
+		QK_GESC,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_6,	/* | */		KC_7,		KC_8,	KC_9,	KC_0,		KC_MINS,	KC_EQL,			KC_BSPC,	/* | */			KC_DEL,
+		KC_TAB,		KC_Q,	CTL_W,	ALT_E,	GUI_R,	KC_T,			/* | */		KC_Y,		GUI_U,	ALT_I,	CTL_O,		KC_P,		KC_LBRC, KC_RBRC, KC_BSLS,	/* | */			KC_HOME,
+		KC_CAPS,	L4_A,	L3_S,	L2_D,	L1_F,	KC_G,			/* | */		KC_H,		L1_J,	L2_K,	L3_L,		L4_SCLN,	KC_QUOT, KC_ENT,			/* | */			KC_END,
+		KC_LSFT,	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,			/* | */		TO(ISRT),	KC_N,	KC_M,	KC_COMM,	KC_DOT,		KC_SLSH, KC_RSFT,			/* | */			KC_UP,
+		KC_LCTL,	KC_LGUI,			   MEH_SPC,	KC_LALT,		/* | */		KC_SPC,		KC_RCTL,													/* | */		KC_LEFT,	KC_DOWN,	KC_RGHT
 	),
 	[GAME] = LAYOUT(
-		QK_GESC,	KC_1,			KC_2,			KC_3,			KC_4,			KC_5,	KC_6,	/* | */		KC_7,			KC_8,			KC_9,			KC_0,				KC_MINS,		    KC_EQL,						KC_BSPC,	/* | */			KC_DEL,
-		KC_TAB,		KC_Q,			KC_W,			KC_E,			KC_R,			KC_T,			/* | */		KC_Y,			KC_U,			KC_I,			KC_O,				KC_P,			    KC_LBRC,		KC_RBRC,	KC_BSLS,	/* | */			KC_PGUP,
-		KC_CAPS,	KC_A,			KC_S,			KC_D,			KC_F,			KC_G,			/* | */		KC_H,			KC_J,			KC_K,			KC_L,				KC_SCLN,		    KC_QUOT,		KC_ENT,					/* | */			KC_PGDN,
-		KC_LSFT,	KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,			/* | */		TO(ISRT),		KC_N,			KC_M,			KC_COMM,			KC_DOT,			    KC_SLSH,		KC_RSFT,				/* | */			KC_UP,
-		KC_LCTL,									KC_LGUI,		KC_SPC,			KC_LALT,		/* | */		KC_RALT,        KC_RCTL,                      											    							/* | */		KC_LEFT,	KC_DOWN,   KC_RIGHT
+		QK_GESC,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_6,	/* | */		KC_7,		KC_8,	KC_9,	KC_0,		KC_MINS,	KC_EQL,					KC_BSPC,	/* | */	KC_DEL,
+		KC_TAB,		KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,			/* | */		KC_Y,		KC_U,	KC_I,	KC_O,		KC_P,		KC_LBRC,	KC_RBRC,	KC_BSLS,	/* | */	KC_PGUP,
+		KC_CAPS,	KC_A,	KC_S,	KC_D,	KC_F,	KC_G,			/* | */		KC_H,		KC_J,	KC_K,	KC_L,		KC_SCLN,	KC_QUOT,	KC_ENT,					/* | */	KC_PGDN,
+		KC_LSFT,	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,			/* | */		TO(ISRT),	KC_N,	KC_M,	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,				/* | */	KC_UP,
+		KC_LCTL,	KC_LGUI,				KC_SPC,	KC_LALT,		/* | */		KC_RALT,	KC_RCTL,													/* | */		KC_LEFT,	KC_DOWN,   KC_RIGHT
 	),
 	[GRPT] = LAYOUT(
-		__X__,		__X__,			__X__,			__X__,			__X__,			__X__,	__X__,	/* | */		__X__,			__X__,			__X__,			__X__,				__X__,			    __X__,						__X__,		/* | */			TO(QWER),
-		__X__,		KC_B,			LCTL_T(KC_L),	LALT_T(KC_D),	LGUI_T(KC_W),	KC_Z,			/* | */		KC_QUOT,		RGUI_T(KC_F),	RALT_T(KC_O),	RCTL_T(KC_U), 	    KC_J,    		    __X__,	 		__X__, 		__X__,		/* | */			TO(NPD),
-		__X__,		LT(NPD, KC_N),	LT(ACCT, KC_R),	LT(SMBL, KC_T),	LT(ACT, KC_S),	KC_G,			/* | */		KC_Y,			LT(ACT, KC_H),	LT(SMBL, KC_A),	LT(ACCT, KC_E),		LT(NPD, KC_I),	    __X__,			__X__,					/* | */			TO(ISRT),
-		__X__,		LSFT_T(KC_Q),	KC_X,			KC_M,			KC_C,			KC_V,			/* | */		__X__,			KC_K,			KC_P,			KC_COMM,		    KC_DOT,	    	    RSFT_T(KC_SLSH),__X__,					/* | */			__X__,
-		__X__,										__X__,			MEH_T(KC_SPC),	QK_LEAD,		/* | */		KC_BSPC,		KC_ENT,																	    							/* | */		__X__,		__X__,		__X__
+		__X__,	__X__,  __X__,	__X__,  __X__,  __X__,	__X__,	/* | */		__X__,		__X__,	__X__,	__X__,		__X__,	__X__,              __X__,		/* | */     TO(QWER),
+		__X__,	KC_B,   CTL_L,	ALT_D,  GUI_W,  KC_Z,			/* | */		KC_QUOT,	GUI_F,	ALT_O,	CTL_U,		KC_J,	__X__,	    __X__,  __X__,		/* | */     TO(NPD),
+		__X__,	L4_N,   L3_R,	L2_T,   L1_S,   KC_G,			/* | */		KC_Y,		L1_H,	L2_A,	L3_E,		L4_I,	__X__,	    __X__,              /* | */     TO(ISRT),
+		__X__,	SFT_Q,	KC_X,   KC_M,   KC_C,   KC_V,			/* | */		__X__,		KC_K,	KC_P,	KC_COMM,	KC_DOT,	SFT_SLSH,   __X__,              /* | */     __X__,
+		__X__,  __X__,				   MEH_SPC,	QK_LEAD,	    /* | */		KC_BSPC,	KC_ENT,														/* | */		__X__,  __X__,		__X__
 	),
 	[ACT] = LAYOUT(
 		__X__,    	__X__,			__X__,          __X__,          __X__,          __X__,	__X__,	        /* | */		__X__,			__X__,			__X__,			__X__,				__X__,			__X__,		    				__X__,		/* | */			__X__,
@@ -51,22 +52,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		__X__,   	 __X__,			__X__,          __X__,          __X__,          __X__,	__X__,	/* | */		__X__,			__X__,			__X__,			__X__,				__X__,			__X__,		    				__X__,		/* | */			__X__,
 		__X__,		KC_CAPS,        KC_AT,          KC_HASH,		KC_DOLLAR,      CKC_EURO,		/* | */		__X__,	        KC_LBRC,        KC_RBRC,		KC_SCLN,			__X__,          __X__,		    	__X__,		__X__,		/* | */			__X__,
 		__X__,		KC_GRV,         KC_UNDS,        KC_TILDE,		KC_MINS,        KC_PLUS,		/* | */		KC_EQL,	        KC_LPRN,        KC_RPRN,		KC_COLN,			__X__,	        __X__,		    	__X__,					/* | */			__X__,
-		__X__,		__X__,        	__X__,			KC_LT,			KC_GT,			__X__,			/* | */		__X__,			KC_LCBR,        KC_RCBR,		KC_BSLS,			__X__,			__X__,		    	__X__,					/* | */			__X__,
-		__X__,										__X__,			__X__,		    __X__,	        /* | */		__X__,	        DF(ISRT),																		    					/* | */		__X__,		__X__,		__X__
+		__X__,		__X__,        	__X__,			KC_LT,			KC_GT,			KC_CIRC,		/* | */		__X__,			KC_LCBR,        KC_RCBR,		KC_BSLS,			__X__,			__X__,		    	__X__,					/* | */			__X__,
+		__X__,										__X__,			__X__,		    __X__,	        /* | */		__X__,	        TO(ISRT),																		    					/* | */		__X__,		__X__,		__X__
 	),
 	[ACCT] = LAYOUT(
 		__X__,		__X__,			__X__,          __X__,          __X__,          __X__,	__X__,	/* | */		__X__,			__X__,			__X__,			__X__,				__X__,			__X__,                          __X__,  	/* | */			__X__,
 		__X__,		CW_TOGG,      	KC_ASTR,        KC_AMPR,		KC_PERCENT,     SC(CMD_REDO),	/* | */	    __X__,			SC(E_CIRC),     SC(U_GRAVE),	SC(I_CIRC),			SC(I_TREMA),	__X__,		        __X__,		__X__,		/* | */			__X__,
 		__X__,		KC_TAB,         KC_DQUO,        KC_EXLM,		KC_QUES,        SC(CMD_UNDO),	/* | */	    __X__,			SC(E_AIGU),     SC(E_GRAVE),	SC(A_GRAVE),		SC(O_CIRC),	 	__X__,		        __X__,					/* | */			__X__,
-		__X__,		TO(GAME),       TO(QWER),		TO(GRPT),		DF(NPD),	    __X__,      	/* | */	    __X__,			SC(C_CEDILLE),  SC(U_CIRC),		SC(A_CIRC),         __X__,          __X__,              __X__,					/* | */			__X__,
-		__X__,										__X__,			__X__,		    KC_LSFT,        /* | */		__X__,	        DF(ISRT),																							/* | */		__X__,		__X__,		__X__
+		__X__,		TO(GAME),       TO(QWER),		TO(GRPT),		TO(NPD),	    __X__,      	/* | */	    __X__,			SC(C_CEDILLE),  SC(U_CIRC),		SC(A_CIRC),         __X__,          __X__,              __X__,					/* | */			__X__,
+		__X__,										__X__,			__X__,		    KC_LSFT,        /* | */		__X__,	        TO(ISRT),																							/* | */		__X__,		__X__,		__X__
 	),
 	[NPD] = LAYOUT( // multimedia is to do
 		__X__,		__X__,			__X__,          __X__,          __X__,          __X__,	__X__,	/* | */		__X__,			__X__,			__X__,			__X__,				__X__,			__X__,			    			__X__,		/* | */			QK_BOOT,
 		__X__,		KC_PAST,        KC_P7,          KC_P8,			KC_P9,          KC_PPLS,		/* | */	    RGB_TOG,		RGB_MOD,		RGB_RMOD,		RGB_SAI,		    RGB_SAD,		__X__,			    __X__,		__X__,		/* | */			__X__,
 		__X__,		KC_P0,          KC_P4,          KC_P5,			KC_P6,          KC_PENT,		/* | */	    KC_PSCR,		RGB_HUI,		RGB_HUD,		RGB_VAI,		    RGB_VAD,		__X__,			    __X__,					/* | */			__X__,
 		__X__,		KC_PSLS,         KC_P1,          KC_P2,			KC_P3,          KC_PMNS,		/* | */	    KC_MNXT,		KC_VOLU,		KC_VOLD,		RGB_SPI,			RGB_SPD,		__X__,			    __X__,					/* | */			__X__,
-		__X__,										__X__,			KC_EQL,			KC_DOT,			/* | */	    KC_MPLY,	    DF(ISRT),																			    					/* | */		__X__,		__X__,		__X__
+		__X__,										__X__,			KC_ENT,			KC_DOT,			/* | */	    KC_MPLY,	    TO(ISRT),																			    					/* | */		__X__,		__X__,		__X__
 	)
 };
 
@@ -93,35 +94,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    for (uint8_t i = led_min; i < led_max; i++) {
-        switch (get_highest_layer(layer_state | default_layer_state)) {
-            case ISRT:
-                rgb_matrix_set_color(i, RGB_WHITE);
-                break;
-            case QWER:
-                rgb_matrix_set_color(i, RGB_GOLD);
-                break;
-            case GAME:
-                rgb_matrix_set_color(i, RGB_RED);
-                break;
-            case GRPT:
-                rgb_matrix_set_color(i, RGB_GREEN);
-                break;
-            case ACT:
-                rgb_matrix_set_color(i, RGB_BLUE);
-                break;
-            case SMBL:
-                rgb_matrix_set_color(i, RGB_AZURE);
-                break;
-            case ACCT:
-                rgb_matrix_set_color(i, RGB_CYAN);
-                break;
-            case NPD:
-                rgb_matrix_set_color(i, HSV_SPRINGGREEN);
-                break;
-            default:
-                break;
-        }
-    }
-    return false;
+	for (uint8_t i = led_min; i < led_max; i++) {
+		switch (get_highest_layer(layer_state | default_layer_state)) {
+			case ISRT:
+				rgb_matrix_set_color(i, RGB_WHITE);
+				break;
+			case QWER:
+				rgb_matrix_set_color(i, RGB_GOLD);
+				break;
+			case GAME:
+				rgb_matrix_set_color(i, RGB_RED);
+				break;
+			case GRPT:
+				rgb_matrix_set_color(i, RGB_GREEN);
+				break;
+			case ACT:
+				rgb_matrix_set_color(i, RGB_BLUE);
+				break;
+			case SMBL:
+				rgb_matrix_set_color(i, RGB_AZURE);
+				break;
+			case ACCT:
+				rgb_matrix_set_color(i, RGB_CYAN);
+				break;
+			case NPD:
+				rgb_matrix_set_color(i, HSV_SPRINGGREEN);
+				break;
+			default:
+				break;
+		}
+	}
+	return false;
 }
